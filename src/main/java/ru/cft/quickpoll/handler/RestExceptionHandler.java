b828f0e6b61e70fb52abc463f813415d0ef31ad4
Alexandr Alifirov <aalifirov@gmail.com>
2022-01-09 22:15:54 +0700

    Сваггер документация

--- src/main/java/ru/cft/quickpoll/handler/RestExceptionHandler.java
+++ src/main/java/ru/cft/quickpoll/handler/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -64,4 +65,15 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(manve, errorDetail, headers, status, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimeStamp(new Date().getTime());
+        errorDetail.setStatus(status.value());
+        errorDetail.setTitle("Message Not Readable");
+        errorDetail.setDetail(ex.getMessage());
+        errorDetail.setDeveloperMessage(ex.getClass().getName());
+
+        return handleExceptionInternal(ex, errorDetail, headers, status, request);
+    }
 }
