a9524fc59c4f19b53476e79e9f142d1a5585c06d
Alexandr Alifirov <aalifirov@gmail.com>
2022-01-15 10:47:48 +0700

    Документация для двух версий

---
+++ src/main/java/ru/cft/quickpoll/v2/controller/PollController.java
@@ -0,0 +1,83 @@
+package ru.cft.quickpoll.v1.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.cft.quickpoll.error.ErrorDetail;
+import ru.cft.quickpoll.exception.ResourceNotFoundException;
+import ru.cft.quickpoll.model.Poll;
+import ru.cft.quickpoll.repository.PollRepository;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
+
+@RestController("pollControllerV1")
+@RequestMapping("/v1")
+@Slf4j
+@Api(value = "polls", description = "Poll API")
+public class PollController {
+
+    @Autowired
+    private PollRepository pollRepository;
+
+    @ApiOperation(value = "Retrieves all polls", response = Poll.class, responseContainer = "List")
+    @GetMapping("/polls")
+    public ResponseEntity<?> getAllPolls(){
+        Iterable<Poll> allPolls = pollRepository.findAll();
+        return new ResponseEntity(allPolls, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Creates a new Poll", notes = "The newly created poll Id will be sent in the response header", response = Void.class)
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Poll Created Successfully", response = Void.class),
+                           @ApiResponse(code = 500,  message = "Error Creating Poll", response = ErrorDetail.class)})
+    @PostMapping("/polls")
+    public ResponseEntity<Void> createPoll(@Valid @RequestBody Poll poll){
+        log.info(String.format("poll with question [%s]", poll.getQuestion()));
+        poll = pollRepository.save(poll);
+
+        // Set the location header for newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(poll.getId())
+                .toUri();
+        responseHeaders.setLocation(newPollUri);
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "Retrieves a Poll associated with the pollId", response = Poll.class)
+    @GetMapping("/polls/{pollId}")
+    public ResponseEntity getPoll(@PathVariable Long pollId) throws Exception{
+        return new ResponseEntity(verifyPoll(pollId), HttpStatus.OK);
+    }
+
+    @PutMapping("/polls/{pollId}")
+    public ResponseEntity<?> updatePoll(@RequestBody Poll poll,  @PathVariable Long pollId){
+        verifyPoll(pollId);
+        pollRepository.save(poll);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/polls/{pollId}")
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId){
+        verifyPoll(pollId);
+        pollRepository.deleteById(pollId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    protected Poll verifyPoll(Long pollId){
+        Optional<Poll> poll = pollRepository.findById(pollId);
+        if (!poll.isPresent()) { throw new ResourceNotFoundException("Poll with id " + pollId + " not found");}
+        return poll.get();
+    }
+}
